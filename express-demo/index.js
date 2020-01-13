22b33b8de276e0faece2b2c5c67560c36fc99011
Misael Burboa <mburboa@pipingrock.com>
2020-01-13 11:55:52 -0700

    validate, add and update an entry

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,4 +1,6 @@
+const Joi = require('joi');
 const express = require('express');
+
 const app = express();
 
 app.use(express.json());
@@ -30,6 +32,12 @@ app.get('/api/courses/:id', (req, res) => {
 });
 
 app.post('/api/courses', (req, res) => {
+    const { error } = validateCourse(req.body);
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
     const course = {
         id: courses.length + 1,
         name: req.body.name
@@ -38,6 +46,32 @@ app.post('/api/courses', (req, res) => {
     courses.push(course);
     res.send(course);
 });
+
+app.put('/api/courses/:id', (req, res) => {
+    let course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) {
+        res.status(404).send('The course with the given ID was not found');
+        return;
+    }
+
+    const { error } = validateCourse(req.body);
+    if (error) {
+        // 400 Bad request
+        res.status(400).send(error.details[0].message);
+        return;
+    }
+
+    course.name = req.body.name;
+    res.send(course);
+});
+
+function validateCourse(course) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+
+    return Joi.validate(course, schema);
+}
 
 // PORT
 const port = process.env.PORT || 3000;
